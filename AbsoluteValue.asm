be8a55547f00e5362ceba14211ed81f17a48b1e9
dianalleo <rhnx2502@leeds.ac.uk>
2025-03-04 16:48:17 +0000

    Update AbsoluteValue.asm

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,33 +1,33 @@
-@0
+@0 //get x
 D=M
 @1
-M=D
+M=D // put x into reg1 
 @POSITIVE
-D;JGE
+D;JGE // if x is positive, go to positive func
 @2
-M=1
-@32768
+M=1 // setting r2 to 1 if x is negative
+@32768 // max num?
 D=A
 @CHECK_OVERFLOW
-D=D-M
-D;JEQ
+D=D-M // d = 32768 - x?????
+D;JEQ // if zero go to check overflow
 @0
-D=M
-D=-D
+D=M // get x
+D=-D // invert x
 @1
-M=D
+M=D // set r1 to 1 (result, absol val of x) this is normal case
 @END
-0;JMP
+0;JMP //end program now
 (POSITIVE)
 @2
-M=0
+M=0 //set r2 as 0 bc x is pos
 @3
-M=0
+M=0 // operation can be done, r3 is 0
 @END
-0;JMP
-(CHECK_OVERFLOW)
+0;JMP //end
+(CHECK_OVERFLOW) // just sets r3 as 1 if x if bigger than the max
 @3
-M=1
+M=1 // cant be done
 @END
 0;JMP
 (END)
