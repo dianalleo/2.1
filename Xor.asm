7bd5e0189c114666a4944fa77ed8b1cccba79be1
dianalleo <rhnx2502@leeds.ac.uk>
2025-04-30 16:27:32 +0100

    Update Xor.asm

--- Xor.asm
+++ Xor.asm
@@ -1,18 +1,18 @@
-@0
-D=M
-@1
-D=D|M
-@3
-M=D
-@0
-D=M
-@1
-D=D&M
+@0 // the first value used in the XOR operation is stored in register 0
+D=M // the value from register 0 is moved to the D (data) register
+@1 // the second value used in the XOR operation is stored in M (memory)
+D=D|M // bitwise OR operation performed using the values in D and M registers, and then the result is stored in D
+@3 // accessing register 3
+M=D // storing the result of OR operation (currently in D register) in register 3
+@0 // accessing register 0 
+D=M // value in M is stored in register 0 (originally from register 3, the result of OR)
+@1 // going to register 1
+D=D&M // AND operation performed using values from registers 0 and 1 (as these are in D and M), result is stored in D
+@4 // going to register 4
+M=D // storing the result of AND operation in register 4
+@3 
+D=M // storing the value from register 3 in D (result of OR)
 @4
-M=D
-@3
-D=M
-@4
-D=D-M
+D=D-M // XOR can be performed by doing (x OR y) - (x AND y). here D initially holds the result of OR, and M holds the result of AND
 @2
-M=D
+M=D // the final result of the XOR operation is stored in register 2
